dd981a7e894a28c821ad873cd2f171b02ee438b4
root <root@localhost>
2025-08-25 14:36:41 +0000

    chore(repo): remove terraform and modules directories as requested

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = var.tf_state_bucket
-    key            = "terraform.tfstate"
-    region         = var.region
-    dynamodb_table = var.tf_lock_table
-    encrypt        = true
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "site_bucket_name" {
-  description = "Name of the S3 bucket hosting the site"
-  value       = aws_s3_bucket.site.bucket
-}
-
-output "cloudfront_domain_name" {
-  description = "CloudFront distribution domain name"
-  value       = aws_cloudfront_distribution.cdn.domain_name
-}
